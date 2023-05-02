6c859931c2ba56b53b439652f22e2a99a1a86160
anlop31 <analopez31@correo.ugr.es>
2023-05-03 01:27:37 +0200

    practica 3 ejemplos

---
+++ Pr3_RMI/Ejercicio/script.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -e
+# ejecutar = Macro para compilacion y ejecucion del programa ejemplo
+# en una sola maquina Unix de nombre localhost.
+echo
+echo "Lanzando el ligador de RMI ... "
+rmiregistry 1099 &
+echo
+echo "Compilando con javac ..."
+javac *.java
+sleep 2
+echo
+echo "Lanzando el servidor"
+java -cp . -Djava.rmi.server.codebase=file:./ -Djava.rmi.server.hostname=localhost -Djava.security.policy=server.policy servidor &
+sleep 2
+
+echo
+echo "Lanzando el cliente"
+echo
+java -cp . -Djava.security.policy=server.policy cliente
+sleep 2
+#echo
+#echo "Lanzando el segundo cliente"
+#echo
+#java -cp . -Djava.security.policy=server.policy Cliente_Ejemplo localhost 3
